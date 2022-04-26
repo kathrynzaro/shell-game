9e4228582b986cdf15cae10ca7b387d57a4c3398
kathrynzaro <zarokathryn@gmail.com>
2022-04-25 17:27:36 -0700

    grabbed elements and added event listeners

--- app.js
+++ app.js
@@ -7,18 +7,88 @@ const shell1 = document.getElementById('shell-1');
 const shell2 = document.getElementById('shell-2');
 const shell3 = document.getElementById('shell-3');
 
-
+const winSpan = document.getElementById('wins');
+const lossSpan = document.getElementById('losses');
+const totalSpan = document.getElementById('total');
 
 // let state
+let wins = 0;
+let losses = 0;
 
 // set event listeners 
 
 button1.addEventListener('click', () => {
-  shell1.classList.add('reveal');
+    shell1.classList.remove('reveal');
+    shell2.classList.remove('reveal');
+    shell3.classList.remove('reveal');
+
+    const randomShell = Math.ceil(Math.random() * 3);
+    if (randomShell === 1) {
+        shell1.classList.add('reveal');
+        wins++;
+    } else if (randomShell === 2) {
+        shell2.classList.add('reveal');
+        losses++;
+    } else {
+        shell3.classList.add('reveal');
+        losses++
+    }
+
+    winSpan.textContent = wins;
+    lossSpan.textContent = losses;
+    totalSpan.textContent = wins + losses;
+
+});
+
+button2.addEventListener('click', () => {
+  shell1.classList.remove('reveal');
   shell2.classList.remove('reveal');
   shell3.classList.remove('reveal');
 
-})
+  const randomShell = Math.ceil(Math.random() * 3);
+  if (randomShell === 1) {
+      shell1.classList.add('reveal');
+      losses++;
+  } else if (randomShell === 2) {
+      shell2.classList.add('reveal');
+      wins++;
+  } else {
+      shell3.classList.add('reveal');
+      losses++
+  }
+
+  winSpan.textContent = wins;
+  lossSpan.textContent = losses;
+  totalSpan.textContent = wins + losses;
+
+});
+
+
+button3.addEventListener('click', () => {
+  shell1.classList.remove('reveal');
+  shell2.classList.remove('reveal');
+  shell3.classList.remove('reveal');
+
+  const randomShell = Math.ceil(Math.random() * 3);
+  if (randomShell === 1) {
+      shell1.classList.add('reveal');
+      losses++;
+  } else if (randomShell === 2) {
+      shell2.classList.add('reveal');
+      losses++;
+  } else {
+      shell3.classList.add('reveal');
+      wins++
+  }
+
+  winSpan.textContent = wins;
+  lossSpan.textContent = losses;
+  totalSpan.textContent = wins + losses;
+
+});
+
+
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
