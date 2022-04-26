be704c1e53acff42318e073f5e7834fd6c6b5466
kathrynzaro <zarokathryn@gmail.com>
2022-04-25 17:50:41 -0700

    reset button and more styling

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const button1 = document.getElementById('shell-1-button');
 const button2 = document.getElementById('shell-2-button');
 const button3 = document.getElementById('shell-3-button');
+const resetButton = document.getElementById('reset');
 
 const shell1 = document.getElementById('shell-1');
 const shell2 = document.getElementById('shell-2');
@@ -41,50 +42,60 @@ button1.addEventListener('click', () => {
 });
 
 button2.addEventListener('click', () => {
-  shell1.classList.remove('reveal');
-  shell2.classList.remove('reveal');
-  shell3.classList.remove('reveal');
+    shell1.classList.remove('reveal');
+    shell2.classList.remove('reveal');
+    shell3.classList.remove('reveal');
 
-  const randomShell = Math.ceil(Math.random() * 3);
-  if (randomShell === 1) {
-      shell1.classList.add('reveal');
-      losses++;
-  } else if (randomShell === 2) {
-      shell2.classList.add('reveal');
-      wins++;
-  } else {
-      shell3.classList.add('reveal');
-      losses++
-  }
+    const randomShell = Math.ceil(Math.random() * 3);
+    if (randomShell === 1) {
+        shell1.classList.add('reveal');
+        losses++;
+    } else if (randomShell === 2) {
+        shell2.classList.add('reveal');
+        wins++;
+    } else {
+        shell3.classList.add('reveal');
+        losses++
+    }
 
-  winSpan.textContent = wins;
-  lossSpan.textContent = losses;
-  totalSpan.textContent = wins + losses;
+    winSpan.textContent = wins;
+    lossSpan.textContent = losses;
+    totalSpan.textContent = wins + losses;
 
 });
 
 
 button3.addEventListener('click', () => {
+    shell1.classList.remove('reveal');
+    shell2.classList.remove('reveal');
+    shell3.classList.remove('reveal');
+
+    const randomShell = Math.ceil(Math.random() * 3);
+    if (randomShell === 1) {
+        shell1.classList.add('reveal');
+        losses++;
+    } else if (randomShell === 2) {
+        shell2.classList.add('reveal');
+        losses++;
+    } else {
+        shell3.classList.add('reveal');
+        wins++
+    }
+
+    winSpan.textContent = wins;
+    lossSpan.textContent = losses;
+    totalSpan.textContent = wins + losses;
+
+});
+
+resetButton.addEventListener('click', () => {
+  winSpan.textContent = 0;
+  lossSpan.textContent = 0;
+  totalSpan.textContent = 0;
+
   shell1.classList.remove('reveal');
   shell2.classList.remove('reveal');
   shell3.classList.remove('reveal');
-
-  const randomShell = Math.ceil(Math.random() * 3);
-  if (randomShell === 1) {
-      shell1.classList.add('reveal');
-      losses++;
-  } else if (randomShell === 2) {
-      shell2.classList.add('reveal');
-      losses++;
-  } else {
-      shell3.classList.add('reveal');
-      wins++
-  }
-
-  winSpan.textContent = wins;
-  lossSpan.textContent = losses;
-  totalSpan.textContent = wins + losses;
-
 });
 
 
